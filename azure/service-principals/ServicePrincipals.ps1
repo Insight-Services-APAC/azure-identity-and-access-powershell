6c0ef510d3a6712fb2972ff63eafc8a34c457cd4
Chris Dymond <chris.dymond@insight.com>
2021-04-19 09:23:43 +0800

    ServicePrincipal output to CSV
    CSV output for Service Principals that have assigned users, groups or other SPs attached

--- azure/service-principals/ServicePrincipals.ps1
+++ azure/service-principals/ServicePrincipals.ps1
@@ -1,8 +1,20 @@
+using namespace Microsoft.Open.Azure.AD.CommonLibrary
+using namespace System.Collections.Generic
+
+$ErrorActionPreference = "Stop"
+
 # Getting service principals that have at least a user or other service principal assignment
 # Useful for finding in use SPs
 # Chris Dymond
 
 # TODO: CSV Output and proofing
+
+try {
+    Get-AzureADCurrentSessionInfo | Out-Null
+}
+catch [AadNeedAuthenticationException] {
+    Connect-AzureAD
+}
 
 class ServiceAppRoleAssignments {
     [string]$ResourceId
@@ -13,25 +25,27 @@ class ServiceAppRoleAssignments {
     [string]$AssignedPrincipalTypes
 }
 
-#[List[ServiceAppRoleAssignments]] $ServicePrincipals = [List[ServiceAppRoleAssignments]]::new()
+[List[ServiceAppRoleAssignments]] $ServiceAppRoleAssignmentsList = [List[ServiceAppRoleAssignments]]::new()
 
-Get-AzureADServicePrincipal -All $true | Where-Object {$_.AccountEnabled -eq $true}  | ForEach-Object {
+Get-AzureADServicePrincipal -All $true | Where-Object { $_.AccountEnabled -eq $true }  | ForEach-Object {
     [ServiceAppRoleAssignments] $ServiceAppRoleAssignments = [ServiceAppRoleAssignments]::new()
     $ServiceAppRoleAssignments.ResourceId = $_.ObjectId
     $ServiceAppRoleAssignments.ResourceType = $_.ObjectType
     $ServiceAppRoleAssignments.ResourceDisplayName = $_.DisplayName
     $ServiceAppRoleAssignment = $_ | Get-AzureADServiceAppRoleAssignment 
 
-    $UserPrincipals = $ServiceAppRoleAssignment | Where-Object {$_.PrincipalType -eq 'User'} | Select-Object PrincipalId
+    $UserPrincipals = $ServiceAppRoleAssignment | Where-Object { $_.PrincipalType -eq 'User' } | Select-Object PrincipalId
     $UserPrincipalNames = @();
     $UserPrincipals | ForEach-Object {
         $UserPrincipalName = Get-AzureADUser -ObjectId $_.PrincipalId | Select-Object UserPrincipalName
         $UserPrincipalNames += $UserPrincipalName
     }
-    $ServiceAppRoleAssignments.AssignedOtherPrincipalNames = ($ServiceAppRoleAssignment | Where-Object {$_.PrincipalType -ne 'User'} | Select-Object -ExpandProperty PrincipalDisplayName ) -join ', '
+    $ServiceAppRoleAssignments.AssignedOtherPrincipalNames = ($ServiceAppRoleAssignment | Where-Object { $_.PrincipalType -ne 'User' } | Select-Object -Unique -ExpandProperty PrincipalDisplayName ) -join ', '
     $ServiceAppRoleAssignments.AssignedUserPrincipalNames = ($UserPrincipalNames | Select-Object -ExpandProperty UserPrincipalName) -join ', '
     $ServiceAppRoleAssignments.AssignedPrincipalTypes = ($ServiceAppRoleAssignment | Select-Object -Unique -ExpandProperty PrincipalType) -join ', '
     if ($ServiceAppRoleAssignments.AssignedUserPrincipalNames -ne '' -or $ServiceAppRoleAssignments.AssignedOtherPrincipalNames -ne '' ) {
-        $ServiceAppRoleAssignments
+        $ServiceAppRoleAssignmentsList.Add($ServiceAppRoleAssignments)
     }
 }
+
+$ServiceAppRoleAssignmentsList.GetEnumerator() | Export-Csv 'ServicePrincipals.csv' -NoTypeInformation
