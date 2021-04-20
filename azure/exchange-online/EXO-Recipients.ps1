6d5bb91692f5f0b38f3372d1834b1e787e7ffbd6
Chris Dymond <chris.dymond@insight.com>
2021-04-20 12:07:12 +0800

    refactor of exchange for simplicity
    removed duplicate calls, dynamic list built using array of recipient types

--- azure/exchange-online/EXO-Recipients.ps1
+++ azure/exchange-online/EXO-Recipients.ps1
@@ -16,13 +16,12 @@ If ($isConnected -ne "True") {
 
 <#
     .SYNOPSIS
-
         @Author: Chris Dymond
     .DESCRIPTION
 
+    TODO: Refactor, filter on type
+
 #>
-
-
 
 # -ResultSize (default 1000) or use Unlimited
 
@@ -48,104 +47,41 @@ $exoRecipients = Get-EXORecipient -ResultSize $ResultSize
 
 Write-Output "$(Get-Date) Completed with $($exoRecipients.Count) results."
 
-# RecipientTypeDetails Lists
-[List[PSCustomObject]] $DiscoveryMailboxes = [List[PSCustomObject]]::new()
-[List[PSCustomObject]] $DynamicDistributionGroups = [List[PSCustomObject]]::new()
-[List[PSCustomObject]] $EquipmentMailboxes = [List[PSCustomObject]]::new()
-[List[PSCustomObject]] $GroupMailboxes = [List[PSCustomObject]]::new()
-[List[PSCustomObject]] $GuestMailUsers = [List[PSCustomObject]]::new()
-[List[PSCustomObject]] $LegacyMailboxes = [List[PSCustomObject]]::new()
-[List[PSCustomObject]] $LinkedMailboxes = [List[PSCustomObject]]::new()
-[List[PSCustomObject]] $LinkedRoomMailboxes = [List[PSCustomObject]]::new()
-[List[PSCustomObject]] $MailContacts = [List[PSCustomObject]]::new()
-[List[PSCustomObject]] $MailForestContacts = [List[PSCustomObject]]::new()
-[List[PSCustomObject]] $MailNonUniversalGroups = [List[PSCustomObject]]::new()
-[List[PSCustomObject]] $MailUniversalDistributionGroups = [List[PSCustomObject]]::new()
-[List[PSCustomObject]] $MailUniversalSecurityGroups = [List[PSCustomObject]]::new()
-[List[PSCustomObject]] $MailUsers = [List[PSCustomObject]]::new()
-[List[PSCustomObject]] $PublicFolders = [List[PSCustomObject]]::new()
-[List[PSCustomObject]] $PublicFolderMailboxes = [List[PSCustomObject]]::new()
-[List[PSCustomObject]] $RemoteEquipmentMailboxes = [List[PSCustomObject]]::new()
-[List[PSCustomObject]] $RemoteRoomMailboxes = [List[PSCustomObject]]::new()
-[List[PSCustomObject]] $RemoteSharedMailboxes = [List[PSCustomObject]]::new()
-[List[PSCustomObject]] $RemoteTeamMailboxes = [List[PSCustomObject]]::new()
-[List[PSCustomObject]] $RemoteUserMailboxes = [List[PSCustomObject]]::new()
-[List[PSCustomObject]] $RoomLists = [List[PSCustomObject]]::new()
-[List[PSCustomObject]] $RoomMailboxes = [List[PSCustomObject]]::new()
-[List[PSCustomObject]] $SchedulingMailboxes = [List[PSCustomObject]]::new()
-[List[PSCustomObject]] $SharedMailboxes = [List[PSCustomObject]]::new()
-[List[PSCustomObject]] $TeamMailboxes = [List[PSCustomObject]]::new()
-[List[PSCustomObject]] $UserMailboxes = [List[PSCustomObject]]::new()
 
-Write-Output "$(Get-Date) Sorting recipient types"
-
-ForEach ($exoRecipient in $exoRecipients) {
-    switch ($exoRecipient.RecipientTypeDetails) {
-        "DiscoveryMailbox" { $DiscoveryMailboxes.Add($exoRecipient) }
-        "DynamicDistributionGroup" { $DynamicDistributionGroups.Add($exoRecipient) }
-        "EquipmentMailbox" { $EquipmentMailboxes.Add($exoRecipient) }
-        "GroupMailbox" { $GroupMailboxes.Add($exoRecipient) }
-        "GuestMailUser" { $GuestMailUsers.Add($exoRecipient) }
-        "LegacyMailbox" { $LegacyMailboxes.Add($exoRecipient) }
-        "LinkedMailbox" { $LinkedMailboxes.Add($exoRecipient) }
-        "LinkedRoomMailbox" { $LinkedRoomMailboxes.Add($exoRecipient) }
-        "MailContact" { $MailContacts.Add($exoRecipient) }
-        "MailForestContact" { $MailForestContacts.Add($exoRecipient) }
-        "MailNonUniversalGroup" { $MailNonUniversalGroups.Add($exoRecipient) }
-        "MailUniversalDistributionGroup" { $MailUniversalDistributionGroups.Add($exoRecipient) }
-        "MailUniversalSecurityGroup" { $MailUniversalSecurityGroups.Add($exoRecipient) }
-        "MailUser" { $MailUsers.Add($exoRecipient) }
-        "PublicFolder" { $PublicFolders.Add($exoRecipient) }
-        "PublicFolderMailbox" { $PublicFolderMailboxes.Add($exoRecipients) }
-        "RemoteEquipmentMailbox" { $RemoteEquipmentMailboxes.Add($exoRecipient) }
-        "RemoteRoomMailbox" { $RemoteRoomMailboxes.Add($exoRecipient) }
-        "RemoteSharedMailbox" { $RemoteSharedMailboxes.Add($exoRecipient) }
-        "RemoteTeamMailbox" { $RemoteTeamMailboxes.Add($exoRecipient) }
-        "RemoteUserMailbox" { $RemoteUserMailboxes.Add($exoRecipient) }
-        "RoomList" { $RoomLists.Add($exoRecipient) }
-        "RoomMailbox" { $RoomMailboxes.Add($exoRecipient) }
-        "SchedulingMailbox" { $SchedulingMailboxes.Add($exoRecipient) }
-        "SharedMailbox" { $SharedMailboxes.Add($exoRecipient) }
-        "TeamMailbox" { $TeamMailboxes.Add($exoRecipient) }
-        "UserMailbox" { $UserMailboxes.Add($exoRecipient) }
-    
-        Default { Throw "Unexpected RecipientTypeDetail $($exoRecipient.RecipientTypeDetails)" }
-    }
-}
-
-Write-Output "$(Get-Date) Completed.`n"
-
-Write-Output "DiscoveryMailboxes`t`t$($DiscoveryMailboxes.Count) "
-Write-Output "DynamicDistributionGroups`t$($DynamicDistributionGroups.Count)"
-Write-Output "EquipmentMailboxes`t`t$($EquipmentMailboxes.Count)"
-Write-Output "GroupMailboxes`t`t`t$($GroupMailboxes.Count)"
-Write-Output "GuestMailUsers`t`t`t$($GuestMailUsers.Count)"
-Write-Output "LegacyMailboxes`t`t`t$($LegacyMailboxes.Count)"
-Write-Output "LinkedMailboxes`t`t`t$($LinkedMailboxes.Count)"
-Write-Output "LinkedRoomMailboxes`t`t$($LinkedRoomMailboxes.Count)"
-Write-Output "MailContacts`t`t`t$($MailContacts.Count)"
-Write-Output "MailForestContacts`t`t$($MailForestContacts.Count)"
-Write-Output "MailNonUniversalGroups`t`t$($MailNonUniversalGroups.Count)"
-Write-Output "MailUniversalDistributionGroups`t$($MailUniversalDistributionGroups.Count)"
-Write-Output "MailUniversalSecurityGroups`t$($MailUniversalSecurityGroups.Count)"
-Write-Output "MailUsers`t`t`t$($MailUsers.Count)"
-Write-Output "PublicFolders`t`t`t$($PublicFolders.Count)"
-Write-Output "PublicFolderMailboxes`t`t$($PublicFolderMailboxes.Count)"
-Write-Output "RemoteEquipmentMailboxes`t$($RemoteEquipmentMailboxes.Count)"
-Write-Output "RemoteRoomMailboxes`t`t$($RemoteRoomMailboxes.Count)"
-Write-Output "RemoteSharedMailboxes`t`t$($RemoteSharedMailboxes.Count)"
-Write-Output "RemoteTeamMailboxes`t`t$($RemoteTeamMailboxes.Count)"
-Write-Output "RemoteUserMailboxes`t`t$($RemoteUserMailboxes.Count)"
-Write-Output "RoomLists`t`t`t$($RoomLists.Count)"
-Write-Output "RoomMailboxes`t`t`t$($RoomMailboxes.Count)"
-Write-Output "SchedulingMailboxes`t`t$($SchedulingMailboxes.Count)"
-Write-Output "SharedMailboxes`t`t`t$($SharedMailboxes.Count)"
-Write-Output "TeamMailboxes`t`t`t$($TeamMailboxes.Count)"
-Write-Output "UserMailboxes`t`t`t$($UserMailboxes.Count)`n"
+$RecipientTypeDetails = @(
+    'DiscoveryMailbox',
+    'DynamicDistributionGroup',
+    'EquipmentMailbox',
+    'GroupMailbox',
+    'GuestMailUser',
+    'LegacyMailbox',
+    'LinkedMailbox',
+    'LinkedRoomMailbox',
+    'MailContact',
+    'MailForestContact',
+    'MailNonUniversalGroup',
+    'MailUniversalDistributionGroup',
+    'MailUniversalSecurityGroup',
+    'MailUser',
+    'PublicFolder',
+    'PublicFolderMailbox',
+    'RemoteEquipmentMailbox',
+    'RemoteRoomMailbox',
+    'RemoteSharedMailbox'
+    'RemoteTeamMailbox',
+    'RemoteUserMailbox',
+    'RoomList',
+    'RoomMailbox',
+    'SchedulingMailbox',
+    'SharedMailbox',
+    'TeamMailbox',
+    'UserMailbox'    
+)
 
 function AddMailboxSizes([List[PSCustomObject]] $Mailboxes) {
     $Mailboxes | ForEach-Object {
         $Statistics = Get-EXOMailboxStatistics $_.Identity
+        $bytesStartIndex = $Statistics.TotalItemSize.Value.ToString().IndexOf('(')
         $_ | Add-Member -NotePropertyName TotalItemSize -NotePropertyValue $Statistics.TotalItemSize -Force
 
         $SizeInGB = [Math]::Round($_.TotalItemSize.Value.ToString().Substring($bytesStartIndex + 1, `
@@ -163,70 +99,45 @@ function SumMailboxSizes ([List[PSCustomObject]] $Mailboxes) {
     $TotalSize
 }
 
-Write-Output "Appending Mailbox Sizes"
+$AllRecipientsList = [List[PSCustomObject]]::new()
 
-AddMailboxSizes $DiscoveryMailboxes
-AddMailboxSizes $EquipmentMailboxes
-AddMailboxSizes $GroupMailboxes
-AddMailboxSizes $LegacyMailboxes
-AddMailboxSizes $LinkedMailboxes
-AddMailboxSizes $LinkedRoomMailboxes
-AddMailboxSizes $PublicFolderMailboxes
-AddMailboxSizes $RemoteEquipmentMailboxes
-AddMailboxSizes $RemoteRoomMailboxes
-AddMailboxSizes $RemoteSharedMailboxes
-AddMailboxSizes $RemoteTeamMailboxes
-AddMailboxSizes $RemoteUserMailboxes
-AddMailboxSizes $RoomMailboxes
-AddMailboxSizes $SchedulingMailboxes
-AddMailboxSizes $SharedMailboxes
-AddMailboxSizes $TeamMailboxes
-AddMailboxSizes $UserMailboxes
-
-function GetCountAndTotalSize ([List[PSCustomObject]] $Mailboxes) {
-    
-    $Count = $Mailboxes.Count
-    
-    $TotalSize = 0
-    $Mailboxes | ForEach-Object {
-        $TotalSize += $_.TotalItemSizeInGB
-    }
-    if ($Count -ne 0) {
-        "$Count $TotalSize GB"
-    }
-    else {
-        $Count
-    }
-    
+$exoRecipients | ForEach-Object {
+    $AllRecipientsList.Add($_)
 }
 
-Write-Output "DiscoveryMailboxes`t`t$(GetCountAndTotalSize $DiscoveryMailboxes)"
-Write-Output "DynamicDistributionGroups`t$($DynamicDistributionGroups.Count)"
-Write-Output "EquipmentMailboxes`t`t$(GetCountAndTotalSize $EquipmentMailboxes)"
-Write-Output "GroupMailboxes`t`t`t$(GetCountAndTotalSize $GroupMailboxes)"
-Write-Output "GuestMailUsers`t`t`t$($GuestMailUsers.Count)"
-Write-Output "LegacyMailboxes`t`t`t$(GetCountAndTotalSize $LegacyMailboxes)"
-Write-Output "LinkedMailboxes`t`t`t$(GetCountAndTotalSize $LinkedMailboxes)"
-Write-Output "LinkedRoomMailboxes`t`t$(GetCountAndTotalSize $LinkedRoomMailboxes)"
-Write-Output "MailContacts`t`t`t$($MailContacts.Count)"
-Write-Output "MailForestContacts`t`t$($MailForestContacts.Count)"
-Write-Output "MailNonUniversalGroups`t`t$($MailNonUniversalGroups.Count)"
-Write-Output "MailUniversalDistributionGroups`t$($MailUniversalDistributionGroups.Count)"
-Write-Output "MailUniversalSecurityGroups`t$($MailUniversalSecurityGroups.Count)"
-Write-Output "MailUsers`t`t`t$($MailUsers.Count)"
-Write-Output "PublicFolders`t`t`t$($PublicFolders.Count)"
-Write-Output "PublicFolderMailboxes`t`t$(GetCountAndTotalSize $PublicFolderMailboxes)"
-Write-Output "RemoteEquipmentMailboxes`t$(GetCountAndTotalSize $RemoteEquipmentMailboxes)"
-Write-Output "RemoteRoomMailboxes`t`t$(GetCountAndTotalSize $RemoteRoomMailboxes)"
-Write-Output "RemoteSharedMailboxes`t`t$($RemoteSharedMailboxes.Count)"
-Write-Output "RemoteTeamMailboxes`t`t$($RemoteTeamMailboxes.Count)"
-Write-Output "RemoteUserMailboxes`t`t$($RemoteUserMailboxes.Count)"
-Write-Output "RoomLists`t`t`t$($RoomLists.Count)"
-Write-Output "RoomMailboxes`t`t`t$(GetCountAndTotalSize $RoomMailboxes)"
-Write-Output "SchedulingMailboxes`t`t$(GetCountAndTotalSize $SchedulingMailboxes)"
-Write-Output "SharedMailboxes`t`t`t$(GetCountAndTotalSize $SharedMailboxes)"
-Write-Output "TeamMailboxes`t`t`t$(GetCountAndTotalSize $TeamMailboxes)"
-Write-Output "UserMailboxes`t`t`t$(GetCountAndTotalSize $UserMailboxes)`n"
+class ExchangeSummaryItem {
+    [string]$Kind
+    [int]$TotalObjects
+    [string]$TotalSizeInGB
+}
+
+$OverviewReport = [List[ExchangeSummaryItem]]::new()
+
+
+$RecipientTypeDetails | ForEach-Object {
+    $esi = [ExchangeSummaryItem]::new()
+    $esi.Kind = $_
+    $Resources = [Linq.Enumerable]::ToList(([Linq.Enumerable]::Where(
+                $AllRecipientsList, [Func[PSCustomObject, bool]] { param($x); return $x.RecipientTypeDetails -eq $_ }
+            )))
+    $esi.TotalObjects = $Resources.Count
+
+    if ($_ -match 'mailbox') {
+        AddMailboxSizes $Resources
+        $TotalSize = 0
+        $Resources | ForEach-Object {
+            $TotalSize += $_.TotalItemSizeInGB
+        }
+        $esi.TotalSizeInGB = $TotalSize
+    }
+    $OverviewReport.Add($esi)
+}
+
+$OverviewReport
+
+$OverviewReport.GetEnumerator() | Export-Csv 'ExchangeOverviewReport.csv' -NoTypeInformation
+
+return
 
 function AddMailboxPermissions([List[PSCustomObject]] $Mailboxes) {
     $Mailboxes | ForEach-Object {
@@ -243,7 +154,7 @@ function AddMailboxPermissions([List[PSCustomObject]] $Mailboxes) {
 }
 
 Write-Output "Adding Mailbox Permissions"
-AddMailboxPermissions $DiscoveryMailboxes
+#AddMailboxPermissions $DiscoveryMailboxes
 #ToDo: Others.
 
 # $EmailQuery = 'SMTP:Chris.Dymond@domain.com'
