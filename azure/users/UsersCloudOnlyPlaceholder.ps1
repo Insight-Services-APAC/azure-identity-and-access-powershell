a182380e9093aae212366df6bc5fe136b9f55a36
Chris Dymond <chris.dymond@insight.com>
2021-04-22 17:26:49 +0800

    csv output for cloud only accounts

--- azure/users/UsersCloudOnlyPlaceholder.ps1
+++ azure/users/UsersCloudOnlyPlaceholder.ps1
@@ -1,4 +1,5 @@
 using namespace System.Collections.Generic
+using namespace Microsoft.Open.Azure.AD.CommonLibrary
 
 $ErrorActionPreference = "Stop"
 
@@ -81,3 +82,56 @@ Write-Output "Cloud Only Users With Mailboxes: $($CloudOnlyUsersWithMailboxes.Co
 Write-Output "Cloud Only Shared Mailboxes: $($CloudOnlySharedMailboxes.Count)"
 Write-Output "Cloud Only Equipment Mailboxes: $($CloudOnlyEquipmentMailboxes.Count)"
 Write-Output "Cloud Only Room Mailboxes: $($CloudOnlyRoomMailboxes.Count)"
+
+class ReportItem {
+    [string]$DisplayName
+    [string]$UserPrincipalName
+    [string]$Type
+}
+
+[List[ReportItem]] $ReportItemList = [List[ReportItem]]::new()
+
+$CloudOnlyUsers | ForEach-Object {
+    $ReportItem = [ReportItem]::new()
+    $ReportItem.DisplayName = $_.DisplayName
+    $ReportItem.UserPrincipalName = $_.UserPrincipalName
+    $ReportItem.Type = "User (No Mailbox)"
+    $ReportItemList.Add($ReportItem)
+}
+
+$CloudOnlyUsersWithMailboxes | ForEach-Object {
+    $ReportItem = [ReportItem]::new()
+    $ReportItem.DisplayName = $_.DisplayName
+    $ReportItem.UserPrincipalName = $_.UserPrincipalName
+    $ReportItem.Type = "User"
+    $ReportItemList.Add($ReportItem)
+}
+
+$CloudOnlySharedMailboxes | ForEach-Object {
+    $ReportItem = [ReportItem]::new()
+    $ReportItem.DisplayName = $_.DisplayName
+    $ReportItem.UserPrincipalName = $_.UserPrincipalName
+    $ReportItem.Type = "Shared Mailbox"
+    $ReportItemList.Add($ReportItem)
+}
+
+$CloudOnlyEquipmentMailboxes | ForEach-Object {
+    $ReportItem = [ReportItem]::new()
+    $ReportItem.DisplayName = $_.DisplayName
+    $ReportItem.UserPrincipalName = $_.UserPrincipalName
+    $ReportItem.Type = "Equipment Mailbox"
+    $ReportItemList.Add($ReportItem)
+}
+
+$CloudOnlyRoomMailboxes | ForEach-Object {
+    $ReportItem = [ReportItem]::new()
+    $ReportItem.DisplayName = $_.DisplayName
+    $ReportItem.UserPrincipalName = $_.UserPrincipalName
+    $ReportItem.Type = "Room Mailbox"
+    $ReportItemList.Add($ReportItem)
+}
+
+$ReportItemList | Export-Csv 'report.csv' -NoTypeInformation
+
+Disconnect-ExchangeOnline -Confirm:$false
+Disconnect-AzureAD
